5339cfa2ae704f5f67c9958a0cb2cccd7ec725d8
Michael Cort <mcort@uw.edu>
2020-09-23 21:11:35 -0700

    First attempt. Added add.jinja2 and a handler in main.py.

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donor, Donation
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/add/', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        donor = Donor(name=request.form['donor'])
+        donor.save()
+        donation = Donation(donor=donor.name,
+                            value=int(request.form['donation']))
+        donation.save()
+        donations = Donation.select()
+        return render_template('donations.jinja2', donations=donations)
+    return render_template('add.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
